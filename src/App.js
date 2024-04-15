4091e81114dd9a3bb6909dc99e0f574044c5519b
Pandega27 <122007293+Pandega27@users.noreply.github.com>
2024-04-15 13:15:15 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { LoginSignup } from './component/LoginSignup/LoginSignup';
+
+function App() {
+  return (
+    <div>
+      <LoginSignup/>
+    </div>
+  );
+}
+
+export default App;
